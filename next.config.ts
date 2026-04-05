cf8dfe480c581e1ca77f803d4168d3ad499b8fb1
nishant baiga <nishantbaiga@gmail.com>
2026-04-05 14:13:51 +0530

    project initialization

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  // Prevents Mongoose from being bundled on the client
+  serverExternalPackages: ["mongoose"],
 };
 
 export default nextConfig;
