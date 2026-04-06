98b32ac4de57113b045f6ce3837a307031d24268
nishant baiga <nishantbaiga@gmail.com>
2026-04-06 12:18:51 +0530

    api doc

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  // Prevents Mongoose from being bundled on the client
-  serverExternalPackages: ["mongoose"],
    allowedDevOrigins: ['192.168.29.15'],
 };
 
